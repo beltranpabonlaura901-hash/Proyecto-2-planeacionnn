274a6cf8a9ea89a69ece2b66f4c14a21051baa14
beltranpabonlaura901-hash <beltranpabonlaura901@gmail.com>
2026-04-19 12:26:37 -0500

    Create componentes.py

---
+++ ui/componentes.py
@@ -0,0 +1,35 @@
+from dash import html, dcc, dash_table
+import dash_bootstrap_components as dbc
+
+# estilos
+CARD = {...}
+LBL = {...}
+INP = {...}
+
+# funciones UI
+def lbl(...):
+    ...
+
+def num(...):
+    ...
+
+def sl(...):
+    ...
+
+def kpi_box(...):
+    ...
+
+def titulo(...):
+    ...
+
+def tbl(...):
+    ...
+
+def btn(...):
+    ...
+
+def status(...):
+    ...
+
+def no_data(...):
+    ...
