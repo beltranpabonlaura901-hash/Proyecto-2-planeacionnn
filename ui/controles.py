caaef911ffd6cb96b2378ccc7d019e1732adcfaa
beltranpabonlaura901-hash <beltranpabonlaura901@gmail.com>
2026-04-19 12:27:03 -0500

    Create controles.py

---
+++ ui/controles.py
@@ -0,0 +1,16 @@
+
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+from core.datos import MESES
+from ui.componentes import lbl, num, sl, btn, status, CARD, LBL, INP
+
+CTRL_DEM
+CTRL_AGR
+CTRL_DESAG
+CTRL_SIM
+CTRL_KPI
+CTRL_SENS
+CTRL_ESC
+CTRL_MAP
+NAV
