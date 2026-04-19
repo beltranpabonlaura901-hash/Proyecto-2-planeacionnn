23cb9030c5ae14cf89bc87f18d0aa548d63d8115
beltranpabonlaura901-hash <beltranpabonlaura901@gmail.com>
2026-04-19 12:27:27 -0500

    Create graficas.py

---
+++ ui/graficas.py
@@ -0,0 +1,6 @@
+import math
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from core.datos import PRODUCTOS, MESES, DEM_HISTORICA, DEM_HH, PROD_COLORS
